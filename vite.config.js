1ebb9e14d5a02aa9ebfcd55b85996e5d96b79be4
sina <abf.sina@gmail.com>
2023-10-11 18:33:04 +0330

    add blog pages to the route system

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,9 @@ export default defineConfig({
         'cashout-en': resolve(__dirname, 'cashout-en.html'),
         'cashout-fa': resolve(__dirname, 'cashout-fa.html'),
         main: resolve(__dirname, 'index.html'),
-        'services-fa': resolve(__dirname, 'services-fa.html')
+        'services-fa': resolve(__dirname, 'services-fa.html'),
+        'blog-en': resolve(__dirname, 'blog-en.html'),
+        'blog-fa': resolve(__dirname, 'blog-fa.html')
       }
     }
   },
